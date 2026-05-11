0e43eb124cee83403a77dbfa05bdef5b64e90cd1
vfxu-nor <ostebaget@gmail.com>
2026-05-11 19:27:52 +0200

    Delete app directory

--- app/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "Pinterest Downloader" };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
